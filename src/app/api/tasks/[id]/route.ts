2f574e1f3482c4885607ab03be7904a1f4ddf712
marroyo <nkwaaaa@gmail.com>
2023-09-26 15:07:20 -0300

    add get task by id

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -1,14 +1,46 @@
+import { prisma } from "@/libs/prisma"
 import { NextResponse } from "next/server"
 
 interface Params {
     params: { id: string }
 }
 
-export function GET(request: Request, { params }: Params) {
-    return NextResponse.json(`Obteniendo tarea ${params.id} `)
+export async function GET(request: Request, { params }: Params) {
+    try {
+        const task = await prisma.task.findFirst({
+            where: { id: Number(params.id) }
+        })
+
+        if (!task) {
+            return NextResponse.json("Task not found")
+        }
+
+        return NextResponse.json(task)
+    } catch (error) {
+        console.error(error)
+    }
+
+    return NextResponse.json(`Task not found`)
 }
-export function PUT(request: Request, { params }: Params) {
-    return NextResponse.json(`Modificando tarea ${params.id} `)
+export async function PUT(request: Request, { params }: Params) {
+    /**
+ * 
+ *  try {
+        const task = await prisma.task.update({
+            where: {
+                id: Number(params.id)
+            }
+        })
+    } catch (error) {
+        console.error(error)
+    }
+ */
+    // const { title, description } = await request.json()
+    // const task = await prisma.task.update({
+    //     where: { id: Number(params.id) },
+    //     data: { title, description }
+    // })
+    // return NextResponse.json(task)
 }
 export function DELETE(request: Request, { params }: Params) {
     return NextResponse.json(`Eleminando tarea ${params.id} `)
