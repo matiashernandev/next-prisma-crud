e908cc22ef2ddaadeb2de2d58b8dd8e1c0b3cb48
nkwaaaa <nkwaaaa@gmail.com>
2023-09-27 11:25:56 -0300

    add update and delete task

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -2,20 +2,40 @@
 
 import axios from "axios"
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 import { useForm } from "react-hook-form"
 
-export default function NewPage() {
-  const { register, handleSubmit } = useForm()
-
+export default function NewPage({ params }: { params: { id: string } }) {
+  const { register, handleSubmit, setValue } = useForm()
   const router = useRouter()
 
+  useEffect(() => {
+    if (params.id) {
+      axios.get(`/api/tasks/${params.id}`).then((res) => {
+        setValue("title", res.data.title)
+        setValue("description", res.data.description)
+      })
+    }
+  }, [])
+
   const onSubmit = handleSubmit(async (data) => {
-    const res = await axios.post("/api/tasks", data)
+    if (params.id) {
+      await axios.put(`/api/tasks/${params.id}`, data)
+    } else {
+      await axios.post("/api/tasks", data)
+    }
 
     router.push("/")
-
-    console.log(res)
+    router.refresh()
   })
+
+  const handleDelete = async () => {
+    if (confirm("Are you sure?")) {
+      await axios.delete(`/api/tasks/${params.id}`)
+      router.push("/")
+      router.refresh()
+    }
+  }
 
   return (
     <section className="h-screen flex items-center justify-center">
@@ -33,9 +53,23 @@ export default function NewPage() {
           className="px-3 py-1 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-1 focus:ring-sky-300 focus:border-sky-300 text-black block w-full"
         ></textarea>
 
-        <button className="bg-sky-500 px-3 py-1 rounded-md text-white mt-2">
-          Create
-        </button>
+        <div className="flex justify-between">
+          <button className="bg-sky-500 px-3 py-1 rounded-md text-white mt-2">
+            {params.id ? "Update" : "Create"}
+          </button>
+
+          {params.id ? (
+            <button
+              type="button"
+              onClick={handleDelete}
+              className="bg-red-500 px-3 py-1 rounded-md text-white mt-2"
+            >
+              Delete
+            </button>
+          ) : (
+            ""
+          )}
+        </div>
       </form>
     </section>
   )
